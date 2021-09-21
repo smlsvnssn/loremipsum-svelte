dc48e29973366cbc3425b634f2c3c2d1c5f18aa6
smlsvnssn <samuel@lhli.net>
2021-09-21 23:25:10 +0200

    fixed service worker

--- public/sw.js
+++ public/sw.js
@@ -1,47 +1,44 @@
-const 
-    currentCache = "V4", 
-      
+const
+    currentCache = "V5",
+
     files = [
         './index.html',
         './style.css',
-        './main.js',
-        './imports/nyord.js',
-        './imports/löremIpsum.js',
-        './imports/öQuery.min.js',
+        './build/bundle.js',
     ],
-    
+
     cacheOnInstall = async e => {
         const cache = await caches.open(currentCache);
         cache.addAll(files);
     },
-      
+
     flushOldCaches = async e => {
         for (const name of await caches.keys())
-           name !== currentCache && caches.delete(name);
+            name !== currentCache && caches.delete(name);
     },
-      
+
     getResponse = async request => {
         try {
             const cachedResponse = await caches.match(request);
-            
-            if (cachedResponse) 
+
+            if (cachedResponse)
                 return cachedResponse;
 
             const netResponse = await fetch(request),
-                  cache = await caches.open(currentCache);
+                cache = await caches.open(currentCache);
 
             cache.put(request, netResponse.clone());
 
             return netResponse;
-            
+
         } catch (err) {
             console.warn(err)
         }
     };
-    
-self.addEventListener( 'install', cacheOnInstall );
 
-self.addEventListener( 'activate', flushOldCaches );
+self.addEventListener('install', cacheOnInstall);
 
-self.addEventListener( 'fetch', e => e.respondWith(getResponse(e.request)) );
+self.addEventListener('activate', flushOldCaches);
+
+self.addEventListener('fetch', e => e.respondWith(getResponse(e.request)));
 
